e5d680b3483d2038ba8e203edb506536726f6fcb
Matt Bedell <matt@bedell.co>
2021-09-26 11:42:29 -0400

    Use cookies for authed sessions

--- src/server.js
+++ src/server.js
@@ -3,39 +3,10 @@ const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const jwt = require('jsonwebtoken');
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT || 3001;
-
-const authenticateJwt = (authHeader) =>
-  new Promise((resolve, reject) => {
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (!token) {
-      reject(new Error('no token'));
-    } else {
-      jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) {
-          reject(err);
-        }
-
-        resolve(user);
-      });
-    }
-  });
-
-const jwtAuthMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  return authenticateJwt(authHeader)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch(() => {
-      res.status(403).send();
-    });
-};
 
 const app = express();
 
@@ -44,47 +15,62 @@ const dbClient = new MongoClient(process.env.MONGO_URL);
 dbClient.connect();
 const db = dbClient.db(process.env.MONGO_DB);
 
+const sessionExpiryDate = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
+app.use(cookieSession({
+  name: 'session',
+  secret: process.env.SESSION_SECRET,
+  httpOnly: true,
+  sameSite: 'lax',
+  expires: sessionExpiryDate,
+}));
+
+const authSessionMiddleware = (req, res, next) => {
+  if (req.session.username) {
+    return next();
+  }
+
+  return res.status(403).send();
+};
+
 app.use(express.json());
 
 app.get('/', (_, res) => {
   res.send(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.post('/login', async (req, res) => {
+app.post('/auth', async (req, res) => {
   const user = await db.collection('users').findOne({
     $or: [{ username: req.body.username }, { email: req.body.username }],
   });
 
   if (user) {
+    // dangerous to store the password on the 'user' entity but the stakes are low for this app
     const passwordMatch = await bcrypt.compare(
       req.body.password,
       user.password,
     );
 
     if (passwordMatch) {
-      const newJwt = jwt.sign(
-        { username: user.username },
-        process.env.JWT_SECRET,
-      );
-      return res.status(200).json(newJwt);
+      req.session.username = user.username;
+      return res.status(200).send();
     }
   }
 
   return res.status(404).send('Incorrect username or password');
 });
 
-app.put('/guess', jwtAuthMiddleware, (req, res) => {
+app.put('/guess', authSessionMiddleware, (req, res) => {
   const { guess } = req.body;
 
   const total = guess.reduce((all, { percentage }) => all + percentage, 0);
 
-  if (total > 100) {
+  if (total <= 100) {
+    db.collection('users').updateOne({ username: req.session.username }, { $set: { guess } }).then(({ guess: updatedGuess }) => {
+      res.status(200).json(updatedGuess);
+    });
+  } else {
     res.status(400).send('only one whole dog allowed');
   }
-
-  db.collection('users').updateOne({ username: req.user.username }, { $set: { guess } }).then(({ guess: updatedGuess }) => {
-    res.status(200).json(updatedGuess);
-  });
 });
 
 app.listen(PORT, () => console.log(`Listening on: localhost:${PORT}`));
