79dced0f843e597be8f862aef7ce920e93bf589c
Matt Bedell <matt@bedell.co>
2021-09-28 09:55:12 -0400

    Update routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const cookieSession = require('cookie-session');
@@ -9,11 +12,15 @@ const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const api = express.Router();
 
 const dbClient = new MongoClient(process.env.MONGO_URL);
 
 dbClient.connect();
 const db = dbClient.db(process.env.MONGO_DB);
+
+app.use(morgan('dev'));
+app.use(helmet());
 
 const sessionExpiryDate = new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
 app.use(cookieSession({
@@ -32,13 +39,25 @@ const authSessionMiddleware = (req, res, next) => {
   return res.status(403).send();
 };
 
+const getUser = async (uname) => {
+  const user = await db.collection('users').findOne({
+    $or: [{ username: uname }, { email: uname }],
+  });
+
+  if (user) {
+    return { username: user.username, guess: user.guess };
+  }
+
+  return undefined;
+};
+
 app.use(express.json());
 
 app.get('/', (_, res) => {
   res.send(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.post('/auth', async (req, res) => {
+api.post('/auth', async (req, res) => {
   const user = await db.collection('users').findOne({
     $or: [{ username: req.body.username }, { email: req.body.username }],
   });
@@ -52,14 +71,14 @@ app.post('/auth', async (req, res) => {
 
     if (passwordMatch) {
       req.session.username = user.username;
-      return res.status(200).send();
+      return res.status(200).send({ username: user.username });
     }
   }
 
-  return res.status(404).send('Incorrect username or password');
+  return res.status(404).json({ error: 'Incorrect username or password' });
 });
 
-app.put('/guess', authSessionMiddleware, (req, res) => {
+api.put('/guess', authSessionMiddleware, (req, res) => {
   const { guess } = req.body;
 
   const total = guess.reduce((all, { percentage }) => all + percentage, 0);
@@ -72,6 +91,16 @@ app.put('/guess', authSessionMiddleware, (req, res) => {
     res.status(400).send('only one whole dog allowed');
   }
 });
+
+api.get('/user/:username', async (req, res) => {
+  const user = await getUser(req.params.username);
+  if (user) {
+    return res.status(200).json(user);
+  }
+  return res.status(404).send();
+});
+
+app.use('/api', api);
 
 app.listen(PORT, () => console.log(`Listening on: localhost:${PORT}`));
 
