9ba566c60230c43a224627b5c05d24d897c25272
Matt Bedell <matt@bedell.co>
2021-09-28 09:56:25 -0400

    Formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import regeneratorRuntime from "regenerator-runtime";
+import ReactDom from "react-dom";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import App from './components/app.jsx';
+import App from "./components/app.jsx";
 
 ReactDom.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
