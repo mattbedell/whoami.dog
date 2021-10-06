39d58a427a3f500b592506cd7f75e10124598e17
Matt Bedell <matt@bedell.co>
2021-10-06 09:26:45 -0400

    Add user context

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Login from './login.jsx';
 import UserPage from './userPage.jsx';
+import UserDashboard from './userDashboard.jsx';
+import { UserProvider } from '../util/userContext.jsx';
 
 const theme = createTheme();
 
 const App = () => (
   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <Switch>
       <Route path="/login">
-        <Login />
+        <UserProvider>
+          <Login />
+        </UserProvider>
+      </Route>
+      <Route path="/u/:username/dashboard">
+        <UserProvider>
+          <UserDashboard />
+        </UserProvider>
       </Route>
       <Route path="/u/:username">
         <UserPage />
---
+++ src/util/userContext.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    username: "",
+    guesses: [],
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const { user, setUser } = useContext(UserContext);
+  return [user, setUser];
+};
