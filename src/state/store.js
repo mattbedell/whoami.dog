dc5696893adde14e71c4fb86f3b4b399c6359a72
Matt Bedell <matt@bedell.co>
2021-10-31 13:39:38 -0400

    Disable devtools on prod

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import { setupListeners } from '@reduxjs/toolkit/query';
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-import { WAIDApi } from './api.js';
-import auth from './auth.js';
-import guesses from './guesses.js';
+import { WAIDApi } from "./api.js";
+import auth from "./auth.js";
+import guesses from "./guesses.js";
 
 export const store = configureStore({
   reducer: {
@@ -12,9 +12,9 @@ export const store = configureStore({
     auth,
     guesses,
   },
-  middleware: (getDefaultMiddleware) =>  getDefaultMiddleware().concat(WAIDApi.middleware),
-  devTools: process.env.NODE_ENV === 'development',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(WAIDApi.middleware),
+  devTools: process.env.NODE_ENV === "development",
 });
 
 setupListeners(store.dispatch);
-
